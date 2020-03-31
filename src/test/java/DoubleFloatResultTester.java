ca96236b0dfb8c2fe398b8b1c6508dbe0002b346
Ankit Kapoor <ankitkapoor@Ankits-MacBook-Air.local>
2020-03-31 22:01:15 +0530

    Added test case

--- src/test/java/DoubleFloatResultTester.java
+++ src/test/java/DoubleFloatResultTester.java
@@ -22,5 +22,35 @@ public class DoubleFloatResultTester {
 
     }
 
+    @Test
+    public void shouldNotDisplayCorrectItemsBoughtAndFundsDisplay() {
+        double funds = 1.00;
+        int itemsBought = 0;
+        for (double price = 0.10; price <= funds; price += 0.1) {
+            funds -= price;
+            itemsBought++;
+        }
+        //should be 0
+        Assertions.assertEquals(0.3999999999999999, funds);
+        //should be 4 -> 0.10 + 0.20 + 0.30 + 0.40
+        Assertions.assertEquals(3, itemsBought);
+
+
+    }
+
+    @Test
+    public void shouldDisplayCorrectItemsBoughtAndFunds() {
+        BigDecimal funds = new BigDecimal("1.00");
+        final BigDecimal TEN_CENTS = new BigDecimal(".10");
+        int itemsBought = 0;
+
+        for (BigDecimal price = TEN_CENTS; funds.compareTo(price) >= 0; price = price.add(TEN_CENTS)) {
+            funds = funds.subtract(price);
+            itemsBought++;
+        }
+
+        Assertions.assertEquals(new BigDecimal("0.00"), funds);
+        Assertions.assertEquals(4, itemsBought);
+    }
 
 }
